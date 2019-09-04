7b37eedd208a5ec5baa73d61df7063d55349975c
Milan Bhatiya <milanbhatiya3@gmail.com>
2019-09-04 12:06:26 +0530

    added one new function handle turn

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,9 +10,22 @@ if current_player==0:
 elif current_player==1:
     current_player='O'
 
-def display_board(board):
+def display_board():
     print (board[0]+' | '+board[1]+' | '+board[2])
     print (board[3]+' | '+board[4]+' | '+board[5])
     print (board[6]+' | '+board[7]+' | '+board[8])
 
-print (display_board(board))
+def handle_turn(current_player):
+    valid = True
+    print (current_player+"'s turn")
+    position = input("Choose position (1-9): ")
+    while valid:
+        while position not in '1 2 3 4 5 6 7 8 9'.split():
+            position = input("Please choose from (1-9): ")
+        position = int(position)-1
+        if board[position]=='-':
+            valid=False
+        else:
+            print("Occupied")
+        board[position]=current_player
+        display_board()
